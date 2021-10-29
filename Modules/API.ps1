4fba61ddbe4fc57a350f14492b9ccff0ac88cbda
BradRose <login@bradleyrose.co>
2021-10-29 06:06:32 -0600

    Added a few API calls

--- Modules/API.ps1
+++ Modules/API.ps1
@@ -62,7 +62,7 @@ function FGLogin($fgIP, $fgPort)
         "secretkey" = "$($Credentials.GetNetworkCredential().password)";
         }
 
-    $results = Invoke-WebRequest -Method POST -Uri $uri -Body $PostParameters -SessionVariable FortigateSession
+    Invoke-WebRequest -Method POST -Uri $uri -Body $PostParameters -SessionVariable FortigateSession | Out-Null
 
     $fg.session = $FortigateSession
     $fg.cookie = ($FortigateSession.Cookies.GetCookies("https://$($fg.ipAddress)") | Where-Object { $_.name -eq "ccsrftoken" }).value.replace("`"", "")
@@ -84,7 +84,7 @@ function FGLogout($fg)
     #>
 
     $uri = $fg.urlBase + "logout"
-    $results = Invoke-WebRequest -Method POST -Uri $uri
+    Invoke-WebRequest -Method POST -Uri $uri | Out-Null
 
 }
 
@@ -124,12 +124,50 @@ function Set-SystemGlobalSettings($fg, $settings)
     $jsonSettings = $settings | ConvertTo-Json -Compress
     Try
     {
-        Invoke-RestMethod $uri -Headers $headers -WebSession $fg.session -Method PUT -Body $jsonSettings -ErrorAction SilentlyContinue
+        return Invoke-RestMethod $uri -Headers $headers -WebSession $fg.session -Method PUT -Body $jsonSettings
     }
     Catch [System.Net.WebException]
     {
-        Write-Host "Connection closed due to admin-sport change! Check JSON data for admin-sport value."
+        Write-Host -BackgroundColor Black -ForegroundColor Cyan "Connection closed due to admin-sport change! Check JSON data for admin-sport value."
     }
+}
+
+
+function Get-SystemGlobalSettings($fg)
+{
+    <#
+        .SYNOPSIS
+        Gets System AutoInstall Settings.
+        .DESCRIPTION
+        Sets system auto installation settings for firmware and configuration via USB stick. 
+            FG# get system auto-install
+        (https://BaseAPIUrl/api/v2/cmdb/system/auto-install)
+        .EXAMPLE
+        Get-SystemAutoInstall $fg
+    #>
+    $uri = $fg.urlBase + "api/v2/cmdb/system/auto-install"
+    return ((Invoke-RestMethod -Method GET $uri -WebSession $fg.session).results | ConvertTo-Json)
+}
+
+function Set-SystemAutoInstall($fg, $settings)
+{
+    <#
+        .SYNOPSIS
+        Sets System AutoInstall Settings.
+        .DESCRIPTION
+        Sets system auto installation settings for firmware and configuration via USB stick. 
+            FG# config system auto-install
+            FG(global)# set X
+        (https://BaseAPIUrl/api/v2/cmdb/system/auto-install)
+        .EXAMPLE
+        $autoInstallSettings = @{"auto-install-config" = "disable";"auto-install-image" = "disable";}
+        Set-SystemAutoInstall $fg $autoInstallSettings
+    #>
+    
+    $headers = @{"Content-Type" = "application/json"; "X-CSRFTOKEN" = $fg.cookie}
+    $uri = $fg.urlBase + "api/v2/cmdb/system/auto-install"
+    $jsonSettings = $settings | ConvertTo-Json -Compress
+    return Invoke-RestMethod $uri -Headers $headers -WebSession $fg.session -Method PUT -Body $jsonSettings
 }
 
 function Get-SystemStatus($fg)
@@ -146,6 +184,177 @@ function Get-SystemStatus($fg)
 
     $uri = $fg.urlBase + "api/v2/monitor/system/status"
     return Invoke-RestMethod -Method GET $uri -WebSession $fg.session
+}
+
+function Get-SystemDNS($fg)
+{
+    <#
+        .SYNOPSIS
+        Gets system DNS settings.
+        .DESCRIPTION
+        Sets system DNS settings. 
+            FG# get system dns
+        (https://BaseAPIUrl/api/v2/cmdb/system/dns)
+        .EXAMPLE
+        Get-SystemAutoInstall $fg
+    #>
+    $uri = $fg.urlBase + "api/v2/cmdb/system/dns"
+    return ((Invoke-RestMethod -Method GET $uri -WebSession $fg.session).results | ConvertTo-Json)
+}
+
+function Set-SystemDNS($fg, $settings)
+{
+    <#
+        .SYNOPSIS
+        Sets System DNS settings.
+        .DESCRIPTION
+        Sets system DNS settings. 
+            FG# config system dns
+            FG(global)# set X
+        (https://BaseAPIUrl/api/v2/cmdb/system/dns)
+        .EXAMPLE
+        $dnsSettings = @{"primary" = "10.69.69.69";"secondary" = "192.168.69.69";"domain" = @({"domain" = "domain.name";})}
+        Set-SystemDNS $fg $dnsSettings
+    #>
+    
+    $headers = @{"Content-Type" = "application/json"; "X-CSRFTOKEN" = $fg.cookie}
+    $uri = $fg.urlBase + "api/v2/cmdb/system/dns"
+    $jsonSettings = $settings | ConvertTo-Json -Compress
+    return Invoke-RestMethod $uri -Headers $headers -WebSession $fg.session -Method PUT -Body $jsonSettings
+}
+
+function Get-SystemSettings($fg)
+{
+    <#
+        .SYNOPSIS
+        Gets system settings.
+        .DESCRIPTION
+        Gets system settings. 
+            FG# get system settings
+        (https://BaseAPIUrl/api/v2/cmdb/system/settings)
+        .EXAMPLE
+        Get-SystemSettings $fg
+    #>
+
+    $uri = $fg.urlBase + "api/v2/cmdb/system/settings"
+    return ((Invoke-RestMethod -Method GET $uri -WebSession $fg.session).results | ConvertTo-Json)
+}
+
+function Set-SystemSettings($fg, $settings)
+{
+    <#
+        .SYNOPSIS
+        Sets System settings.
+        .DESCRIPTION
+        Sets system settings.
+            FG# config system settings
+            FG(global)# set X
+        (https://BaseAPIUrl/api/v2/cmdb/system/settings)
+        .EXAMPLE
+        $systemSettings = @{"gui-ap-profile" = "disable";}
+        Set-SystemSettings $fg $systemSettings
+    #>
+    
+    $headers = @{"Content-Type" = "application/json"; "X-CSRFTOKEN" = $fg.cookie}
+    $uri = $fg.urlBase + "api/v2/cmdb/system/settings"
+    $jsonSettings = $settings | ConvertTo-Json -Compress
+    return Invoke-RestMethod $uri -Headers $headers -WebSession $fg.session -Method PUT -Body $jsonSettings
+}
+function Get-DHCPServer($fg)
+{
+    <#
+        .SYNOPSIS
+        Gets DHCP server settings.
+        .DESCRIPTION
+        Gets DHCP server settings. 
+            FG# get system dns
+        (https://BaseAPIUrl/api/v2/cmdb/system.dhcp/server)
+        .EXAMPLE
+        Get-DHCPServer $fg
+    #>
+    $uri = $fg.urlBase + "api/v2/cmdb/system.dhcp/server"
+    return ((Invoke-RestMethod -Method GET $uri -WebSession $fg.session).results | ConvertTo-Json)
+}
+
+function Get-SNMPSysInfo($fg)
+{
+    <#
+        .SYNOPSIS
+        Gets SNMP sysinfo settings.
+        .DESCRIPTION
+        Gets SNMP sysinfo settings. 
+            FG# get system snmp sysinfo
+        (https://BaseAPIUrl/api/v2/cmdb/system.snmp/sysinfo)
+        .EXAMPLE
+        Get-SNMPSysInfo $fg
+    #>
+    $uri = $fg.urlBase + "api/v2/cmdb/system.snmp/sysinfo"
+    return ((Invoke-RestMethod -Method GET $uri -WebSession $fg.session).results | ConvertTo-Json)
+}
+
+function Get-SNMPUser($fg)
+{
+    <#
+        .SYNOPSIS
+        Gets SNMP user settings.
+        .DESCRIPTION
+        Gets SNMP user settings. 
+            FG# get system snmp user
+        (https://BaseAPIUrl/api/v2/cmdb/system.snmp/user)
+        .EXAMPLE
+        Get-SNMPUser $fg
+    #>
+    $uri = $fg.urlBase + "api/v2/cmdb/system.snmp/user"
+    return ((Invoke-RestMethod -Method GET $uri -WebSession $fg.session).results | ConvertTo-Json)
+}
+
+function Set-SNMPSysInfo($fg, $settings)
+{
+    <#
+        .SYNOPSIS
+        Sets SNMP sysinfo settings.
+        .DESCRIPTION
+        Sets SNMP sysinfo settings. 
+            FG# config system snmp sysinfo
+            FG (sysinfo) # set X
+        (https://BaseAPIUrl/api/v2/cmdb/system.snmp/sysinfo)
+        .EXAMPLE
+        $snmpSysInfo = @{"status" = "enable";}
+        Set-SNMPSysInfo $fg $snmpSysInfo
+    #>
+
+    $headers = @{"Content-Type" = "application/json"; "X-CSRFTOKEN" = $fg.cookie}
+    $uri = $fg.urlBase + "api/v2/cmdb/system.snmp/sysinfo"
+    $jsonSettings = $settings | ConvertTo-Json -Compress
+    return Invoke-RestMethod $uri -Headers $headers -WebSession $fg.session -Method PUT -Body $jsonSettings
+}
+
+function Set-SNMPUser($fg, $settings)
+{
+    <#
+        .SYNOPSIS
+        Sets SNMP user settings.
+        .DESCRIPTION
+        Sets SNMP user settings. 
+            FG# config system snmp user
+            FG (user) # edit "snmpUser"
+            FG (snmpUser) # set X
+        (https://BaseAPIUrl/api/v2/cmdb/system.snmp/user)
+        .EXAMPLE
+        $snmpUser = @{"name" = "PCAVPNv3";"status" = "enable";"notify-hosts" = "10.69.69.69";"events" = "intf-ip";}
+        Set-SNMPUser $fg $snmpUser
+    #>
+
+    $headers = @{"Content-Type" = "application/json"; "X-CSRFTOKEN" = $fg.cookie}
+    $jsonSettings = $settings | ConvertTo-Json -Compress
+    $uri = $fg.urlBase + "api/v2/cmdb/system.snmp/user"
+
+    try {Invoke-RestMethod -Method GET ($uri + "/$($settings.name)") -WebSession $fg.session -ErrorAction SilentlyContinue | Out-Null}
+    catch [System.InvalidOperationException] {
+        Write-Host -BackgroundColor Black -ForegroundColor Yellow "SNMP user $($settings.name) does not exist. Creating SNMP user $($settings.name)"
+        return Invoke-RestMethod $uri -Headers $headers -WebSession $fg.session -Method POST -Body $jsonSettings
+    }
+    return Invoke-RestMethod ($uri + "/$($settings.name)") -Headers $headers -WebSession $fg.session -Method PUT -Body $jsonSettings
 }
 
 <#
